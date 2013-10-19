1be8d734aafcb776a476f70718dbac7adb78b631
mgarbacz <mich.garbacz@gmail.com>
2013-10-18 22:10:25 -0500

    Generated Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DrawYourData::Application.config.secret_key_base = '5acca628f55527509236104f5f68e641d799abd163c89d533bcdc7a0a2f1aeb6a1a09520edf74890d6f20728aaf2dc0fd918ae4e3cf783b419b812a78d01c037'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DrawYourData::Application.config.session_store :cookie_store, key: '_draw-your-data_session'
